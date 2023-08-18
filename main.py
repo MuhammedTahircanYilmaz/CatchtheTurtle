2455c52cb253ad44b1961cfdc412d47d40fe3146
Muhammed Tahircan Yılmaz <mtahircanyilmaz@gmail.com>
2023-08-18 15:10:43 +0300

    code revision

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
 from random import randint
 import turtle
 
-game_over = False
-game_length = 10
-points = 0
-randx = 0
-randy = 0
-
-game_screen = turtle.Screen()
-game_screen.title("Catch the Turtle!")
-game_screen.bgcolor("light blue")
-
-the_escapee = turtle.Turtle()
-score_turtle = turtle.Turtle()
-countdown_timer = turtle.Turtle()
-
-# the escapee
-the_escapee.shapesize(1.5)
-the_escapee.penup()
-the_escapee.hideturtle()
-the_escapee.setposition((randint(-225, 225)), (randint(-225, 225)))
-the_escapee.shape("turtle")
-the_escapee.color("green")
-the_escapee.showturtle()
-the_escapee.speed('fastest')
-
-# score
-score_turtle.hideturtle()
-score_turtle.color("black")
-style = ('Courier', 20, 'italic')
-score_turtle.penup()
-score_turtle.goto(-25, 285)
-score_turtle.write(f"Score: {points}", font=style)
-
-# countdown
-countdown_timer.hideturtle()
-countdown_timer.penup()
-countdown_timer.goto(-92, 250)
-countdown_timer.write("Time Left: ", font=style)
-
 
 def turtle_movement():
     global randx, randy
-
     randx = int(randint(-225, 225))
     randy = int(randint(-225, 225))
     the_escapee.goto(randx, randy)
@@ -50,7 +11,6 @@ def turtle_movement():
 
 def count_down():
     global game_over, game_length
-
     if game_length >= 0:
         countdown_timer.clear()
         countdown_timer.write(f"Time Left: {game_length}", font=style)
@@ -72,9 +32,49 @@ def score_up(x, y):
         points += 1
         score_turtle.clear()
         score_turtle.write(f"Score : {points}", font=style)
+        turtle_movement()
 
 
-count_down()
-the_escapee.onclick(score_up)
-game_screen.update()
-turtle.mainloop()
+if __name__ == '__main__':
+    game_over = False
+    game_length = 10
+    points = 0
+    randx = 0
+    randy = 0
+
+    game_screen = turtle.Screen()
+    game_screen.title("Catch the Turtle!")
+    game_screen.bgcolor("light blue")
+
+    the_escapee = turtle.Turtle()
+    score_turtle = turtle.Turtle()
+    countdown_timer = turtle.Turtle()
+
+    # the escapee
+    the_escapee.shapesize(1.5)
+    the_escapee.penup()
+    the_escapee.hideturtle()
+    the_escapee.setposition((randint(-225, 225)), (randint(-225, 225)))
+    the_escapee.shape("turtle")
+    the_escapee.color("green")
+    the_escapee.showturtle()
+    the_escapee.speed('fastest')
+
+    # score
+    score_turtle.hideturtle()
+    score_turtle.color("black")
+    style = ('Courier', 20, 'italic')
+    score_turtle.penup()
+    score_turtle.goto(-25, 285)
+    score_turtle.write(f"Score: {points}", font=style)
+
+    # countdown
+    countdown_timer.hideturtle()
+    countdown_timer.penup()
+    countdown_timer.goto(-92, 250)
+    countdown_timer.write("Time Left: ", font=style)
+
+    count_down()
+    the_escapee.onclick(score_up)
+    game_screen.update()
+    turtle.mainloop()
